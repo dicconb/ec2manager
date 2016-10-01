f5ca736553d3cd7197d6ffadc81b2ed0cfbc133a
Diccon Bate <diccon@dicconbate.co.uk>
2016-10-01 23:27:01 +0100

    set up URLinclusion and templates

--- startstop/views.py
+++ startstop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import boto3
 
 # Create your views here.
+
+
+def list_EC2_instances(request):
+	return render(request, 'startstop/list_EC2_instances.html', {})
