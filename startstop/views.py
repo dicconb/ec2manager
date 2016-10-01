8e63762048dbc9205d2649fdc0676b1c8ff82786
Diccon Bate <diccon@dicconbate.co.uk>
2016-10-01 23:47:52 +0100

    displaying some instance information

--- startstop/views.py
+++ startstop/views.py
@@ -5,4 +5,6 @@ import boto3
 
 
 def list_EC2_instances(request):
-	return render(request, 'startstop/list_EC2_instances.html', {})
+	ec2 = boto3.resource('ec2','eu-west-1')
+	instances = ec2.instances.all()
+	return render(request, 'startstop/list_EC2_instances.html', {'instances': instances})
