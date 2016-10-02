322950aeea0c3a870a0b75318f2f612b97b84f73
Diccon Bate <diccon@dicconbate.co.uk>
2016-10-02 14:04:02 +0100

    typo fix in base.html and moved to generic error page

--- startstop/views.py
+++ startstop/views.py
@@ -9,5 +9,5 @@ def list_EC2_instances(request):
 		instances = util.listInstances(region='eu-west-1',nameregex='^dev1-dev.*')
 		return render(request, 'startstop/list_EC2_instances.html', {'instances': instances})
 	else:
-		return render(request, 'startstop/insufficientpermissions.html')
+		return render(request, 'startstop/error.html', {'error': 'Insufficient IAM permissions to start instances. Please check with the administrator.'})
 
